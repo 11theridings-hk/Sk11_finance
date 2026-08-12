344f1561fe8f2cd444c124a3e97e759dfc6a9893
snookerlivehk-elton <snookerlivehk@gmail.com>
2026-08-13 03:42:23 +0800

    remove pg adapter to fix vercel/railway build issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,12 +4,14 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
